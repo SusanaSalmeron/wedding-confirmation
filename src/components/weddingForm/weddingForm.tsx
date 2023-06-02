0b1ee5ad235cc7b45440d29096b8a25d4621cf96
SusanaSalmeron <exdream76@gmail.com>
2023-06-02 16:03:32 +0200

    changes on form design

--- src/components/weddingForm/weddingForm.tsx
+++ src/components/weddingForm/weddingForm.tsx
@@ -1,11 +1,14 @@
 import { FC } from "react";
 import styles from './weddingForm.module.css'
 import { Field, Form, Formik } from "formik";
+import wedding from '../../images/form.jpg';
+import children from '../../images/children.png';
+import vegetarian from '../../images/vegetarian.png';
+import basic from '../../images/basic.png'
 
 
 
-interface WeddingFormProps {
-}
+interface WeddingFormProps { }
 
 interface InitialValues {
     room: boolean,
@@ -27,19 +30,22 @@ const initialValues: InitialValues = {
 }
 
 const menus: Menu[] = [
-    { name: "Principal", image: "" },
-    { name: "Infantil", image: "" },
-    { name: "Vegetariano", image: "" }
+    { name: "Principal", image: basic },
+    { name: "Infantil", image: children },
+    { name: "Veggie", image: vegetarian },
 
 ]
 
 const WeddingForm: FC<WeddingFormProps> = () => {
     return (
         <div className={styles.wedding} data-testid="wedding">
+            <figure className={styles.photo} >
+                <img src={wedding} alt="wedding"></img>
+            </figure>
             <div className={styles.container}>
                 <div className={styles.group}>
-                    <h2>Hola Susana y Rubén</h2>
-                    <h2>Nº Personas: 2</h2>
+                    <h3>Hola Susana,Rubén</h3>
+                    <h3>Nº Personas: 2</h3>
                 </div>
                 <Formik
                     initialValues={initialValues}
@@ -50,18 +56,21 @@ const WeddingForm: FC<WeddingFormProps> = () => {
                             <label>¿Necesitáis alojamiento?
                                 <Field type="checkbox" name="room" />
                             </label>
-
                         </div>
-                        <div className={styles.menu}>
+                        <div className={styles.menuContainer}>
                             <label >Elije menú para cada uno de los asistentes </label>
-                            {menus.map((menu, i) => {
-                                return <>
-                                    <label >{menu.name}</label>
-                                    <Field key={i} id={i} type="number" min={0} max={2} name={"menu" + i}>
-                                    </Field>
-                                </>
-                            })}
-
+                            <div className={styles.menu}>
+                                {menus.map((menu, i) => {
+                                    return <>
+                                        <figure key={i}>
+                                            <img src={menu.image} alt={menu.name} className={styles.food} />
+                                            <figcaption>{menu.name}</figcaption>
+                                        </figure>
+                                        <Field key={i} id={i} type="number" min={0} max={2} name={"menu" + i}>
+                                        </Field>
+                                    </>
+                                })}
+                            </div>
                         </div>
                         <div className={styles.brunch}>
                             <label>
@@ -71,11 +80,11 @@ const WeddingForm: FC<WeddingFormProps> = () => {
                         </div>
                         <div className={styles.comment}>
                             <label>Comentarios adicionales:</label>
-                            <Field as="textarea" name="comment" rows="10" placeholder="Si se necesita alguna dieta especial por intoleracias o alergias, o cualquier otra observación que no haya sido contemplada en este formulario, aquí puedes reflejarlo para que se tenga en cuenta" />
+                            <Field as="textarea" name="comment" rows="6" placeholder="Si se necesita alguna dieta especial por intoleracias o alergias, o cualquier otra observación que no haya sido contemplada en este formulario, aquí puedes reflejarlo para que se tenga en cuenta" />
                         </div>
                         <div className={styles.warning}>
-                            <h2>ATENCION</h2>
-                            <h3>CONDICIONES: NO ACEPTAMOS REGALOS BAJO NINGUN CONCEPTO</h3>
+                            <h3>ATENCION</h3>
+                            <h4>CONDICIONES: NO ACEPTAMOS REGALOS BAJO NINGUN CONCEPTO</h4>
                             <h5>-AQUELLOS QUE INCUMPLAN LAS CONDICIONES PREVIAMENTE ACEPTADAS SE ARRIESGA A LA PENA DE CATAPULTA DESDE EL CASTILLO-</h5>
                         </div>
                         <div className={styles.terms}>
@@ -89,7 +98,6 @@ const WeddingForm: FC<WeddingFormProps> = () => {
                         </div>
                     </Form>
                 </Formik>
-
             </div>
         </div>
     )
